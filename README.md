b8b8d0accf493b445c07c6354ca18a5d3df9d0e3
kobe577590 <644340980@qq.com>
2017-07-04 01:52:06 +0800

    Python HelloWorld!

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PythonLearn
 python基础教程的代码实现
+
+### 参与者
+- 天蓬元帅
+
+### 课程导读
+- HelloWorld!!
---
+++ helloworld/helloworld.py
@@ -0,0 +1,6 @@
+#coding:utf-8
+'''
+   Run AND Then See The First Python App!! Enjoy
+'''
+if __name__ == '__main__':
+    print ("Python:HelloWolrd!")
